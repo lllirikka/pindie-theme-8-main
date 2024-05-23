df90ab6446afebedb21857e36fde4e6a8fd62c14
lllirikka <kirillsovetov04@gmail.com>
2024-05-23 23:50:58 +0500

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://lllirikka.nomoredomainswork.ru/.api";
+export const BASE_URL = "https://lllirikka.nomoredomainswork.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
